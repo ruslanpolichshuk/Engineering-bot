14278182df664b1e8cae58b507ef5a368f30e880
Ruslan Polichshuk <84552686+ruslanpolichshuk@users.noreply.github.com>
2025-05-30 02:13:17 +0500

    Update config.py

--- rag_assistant/config.py
+++ rag_assistant/config.py
@@ -7,7 +7,7 @@ load_dotenv()  # Загружает переменные из .env в окруж
 
 API_KEY = os.getenv("OPENAI_API_KEY")
 PDF_DIR = "downloaded_pdfs"
-VECTORDB_DIR = "vectordb"
+VECTORDB_DIR = "/vectordb"
 
 if not API_KEY:
     raise ValueError("OPENAI_API_KEY не найден. Убедитесь, что он указан в .env или как переменная окружения.")
